e7568e13ca4dc0d44ea079e76bb560a8a1a1d609
Valentyn Zamykhivskyi <petrobubka.knu@gmail.com>
2025-01-12 00:57:15 +0200

    fix helm

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -7,19 +7,14 @@ terraform {
     tls = {
 
     }
-
-  helm = {
-      source = "hashicorp/helm"
-      version = "~> 2.8.0"
-    }
-
   }
 
 }
-
 
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
 }
+
+

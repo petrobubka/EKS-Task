7d8624cfc6f973c8b8cbf26d1bf4e0a4ffc6d5fc
Valentyn Zamykhivskyi <petrobubka.knu@gmail.com>
2025-01-12 01:22:57 +0200

    fix s3

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "var.bucket_name"
-    key            = "terraform.tfstate"
-    region         = "us-west-1"
-    encrypt        = true
-  }
+  backend "s3" {}
 }

6a5982da569ecf5f25c1d0046a2761bd3730ec4f
Valentyn Zamykhivskyi <petrobubka.knu@gmail.com>
2025-01-12 00:53:12 +0200

    add helm

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -7,6 +7,19 @@ terraform {
     tls = {
 
     }
+
+  helm = {
+      source = "hashicorp/helm"
+      version = "~> 2.8.0"
+    }
+
   }
 
+}
+
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
 }
